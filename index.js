004e3086c5f88d508001bca9d892e48f4022f771
Sergiu333 <anghelenicis59@gmail.com>
2025-01-09 09:56:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/save-data', async (req, res) => {
         } = req.body;
 
         const query = `
-            INSERT INTO test_table2 (TERMINAL, TRTYPE, ORDERR, AMOUNT, CURRENCY, ACTION, RC, APPROVAL, RRN, INT_REF, TIMESTAMP, NONCE, P_SIGN, ECI)
+            INSERT INTO test_table3 (TERMINAL, TRTYPE, ORDERR, AMOUNT, CURRENCY, ACTION, RC, APPROVAL, RRN, INT_REF, TIMESTAMP, NONCE, P_SIGN, ECI)
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
         `;
 
@@ -74,4 +74,25 @@ app.listen(PORT, () => {
 //     P_SIGN TEXT,
 //     ECI TEXT
 // );
+
+// CREATE TABLE test_table3 ( 
+//     TERMINAL TEXT,
+//     TRTYPE TEXT,
+//     "ORDER" TEXT,  
+//     AMOUNT TEXT,
+//     CURRENCY TEXT,
+//     ACTION TEXT,
+//     RC TEXT,
+//     APPROVAL TEXT,
+//     RRN TEXT,
+//     INT_REF TEXT,
+//     TIMESTAMP TEXT,
+//     NONCE TEXT,
+//     P_SIGN TEXT,
+//     ECI TEXT
+// );
+
+
+
+
 
