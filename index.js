db40fa97a018616c2eba9e996224d8909c2f4b3d
Sergiu333 <anghelenicis59@gmail.com>
2025-03-26 13:48:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.post('/save-data', async (req, res) => {
         } = req.body;
 
         const query = `
-            INSERT INTO transaction2 (TERMINAL, TRTYPE, "ORDER", AMOUNT, CURRENCY, ACTION, RC, APPROVAL, RRN, INT_REF, TIMESTAMP, NONCE, P_SIGN, ECI, TEXT)
+            INSERT INTO transaction (TERMINAL, TRTYPE, "ORDER", AMOUNT, CURRENCY, ACTION, RC, APPROVAL, RRN, INT_REF, TIMESTAMP, NONCE, P_SIGN, ECI, TEXT)
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
         `;
 
